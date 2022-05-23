f38075ce689b8876f3bb9d313e8a35793d6166b3
Ben Burdette <bburdette@gmail.com>
2022-05-23 08:01:26 -0600

    remove naersk

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pname = "orgauth";
