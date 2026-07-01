5606ce70d6454b326d43d8b58d245a1a66c2bad2
Ben Burdette <>
2026-07-01 14:05:29 -0600

    update flake, remove 'interface' elms

--- flake.nix
+++ flake.nix
@@ -7,40 +7,41 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
+      system:
+      let
         pname = "orgauth";
         pkgs = nixpkgs.legacyPackages."${system}";
       in
-        rec {
-          inherit pname;
+      rec {
+        inherit pname;
 
-          # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [
-              cargo
-              cargo-watch
-              rustc
-              rustfmt
-              rust-analyzer
-              sqlite
-              pkg-config
-              openssl.dev
-              elm2nix
-              elmPackages.elm
-              elmPackages.elm-analyse
-              elmPackages.elm-doc-preview
-              elmPackages.elm-format
-              elmPackages.elm-live
-              elmPackages.elm-test
-              elmPackages.elm-upgrade
-              elmPackages.elm-xref
-              elmPackages.elm-language-server
-              # elmPackages.elm-verify-examples
-              elmPackages.elmi-to-json
-              elmPackages.elm-optimize-level-2
-            ];
-          };
-        }
+        # `nix develop`
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            cargo
+            cargo-watch
+            rustc
+            rustfmt
+            rust-analyzer
+            sqlite
+            pkg-config
+            openssl.dev
+            elm2nix
+            elmPackages.elm
+            elmPackages.elm-analyse
+            elmPackages.elm-doc-preview
+            elmPackages.elm-format
+            elmPackages.elm-live
+            elmPackages.elm-test
+            elmPackages.elm-upgrade
+            elmPackages.elm-xref
+            elmPackages.elm-language-server
+            # elmPackages.elm-verify-examples
+            # elmPackages.elmi-to-json
+            elmPackages.elm-optimize-level-2
+          ];
+        };
+      }
     );
 }
 
